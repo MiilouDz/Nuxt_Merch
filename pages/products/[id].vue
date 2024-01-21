40aba6c968e106f98732b542cac002a9c7447fca
unknown <miiloudz48@gmail.com>
2024-01-21 19:06:54 +0100

    error handler

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,19 +1,21 @@
 <template>
-    <div>
-      <p>{{ product.title }}</p>
-      <p>{{ product.price }}</p>
-      <p>{{ product.id }}</p>
-    </div>
-  </template>
+  <div>
+    <ProductDetails :product="product" />
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+  const uri = 'https://fakestoreapi.com/products/' + id
+
+  //  fetch the products
+  const { data: product } = await useFetch(uri, { key: id })
   
-  <script setup>
-    const { id } = useRoute().params
-    const uri = 'https://fakestoreapi.com/products/' + id
-  
-    //  fetch the products
-    const { data: product } = await useFetch(uri, { key: id })
-  
-    definePageMeta({
-      layout: "products",
-    })
-  </script>
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found' })
+  }
+
+  definePageMeta({
+    layout: "products",
+  })
+</script>
